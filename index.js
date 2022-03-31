44550beb1a3cfd863ee787af3e923c6aeee6a1b1
psyduck7777 <egor.kolovertnykh@gmail.com>
2022-03-31 03:31:45 +0300

    ssl pt 2

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const https = require('https');
+const fs = require('fs');
+const port = 3000;
 
+var key = fs.readFileSync(__dirname + '/selfsigned.key');
+var cert = fs.readFileSync(__dirname + '/selfsigned.crt');
+var options = {
+  key: key,
+  cert: cert
+};
 
+app = express()
 app.get('/', (req, res) => {
-    return res.send('Hello world')
-})
+   res.send('Now using https..');
+});
 
+var server = https.createServer(options, app);
 
-app.listen(80, () => {
-    console.log('Working')
-})
+server.listen(port, () => {
+  console.log("server starting on port : " + port)
+});
