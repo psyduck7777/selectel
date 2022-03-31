9b5bb90598f14bd5971a7deb43fb77de2c89c37c
psyduck7777 <egor.kolovertnykh@gmail.com>
2022-03-31 03:49:03 +0300

    rmv ssl

--- index.js
+++ index.js
@@ -1,22 +1,12 @@
 const express = require('express');
-const https = require('https');
-const fs = require('fs');
-const port = 3000;
 
-var key = fs.readFileSync(__dirname + '/selfsigned.key');
-var cert = fs.readFileSync(__dirname + '/selfsigned.crt');
-var options = {
-  key: key,
-  cert: cert
-};
 
-app = express()
+const app = express()
+
+
 app.get('/', (req, res) => {
-   res.send('Now using https..');
-});
+    return res.send('Hello world from SELECTEL')
+})
 
-var server = https.createServer(options, app);
 
-server.listen(port, () => {
-  console.log("server starting on port : " + port)
-});
+app.listen(80)
